e3e1b11c6b0b93a0aa374963751bb808f04c6fd7
Syed Muhammad Haris Ahmed <54531180+SyedMuhammadHaris@users.noreply.github.com>
2020-10-05 03:01:22 +0500

    Studio Website Work

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+// $(document).ready(function(){
+//     $(".fancybox").fancybox({
+//           openEffect: "none",
+//           closeEffect: "none"
+//       });
+      
+//       $(".zoom").hover(function(){
+          
+//           $(this).addClass('transition');
+//       }, function(){
+          
+//           $(this).removeClass('transition');
+//       });
+//   });
